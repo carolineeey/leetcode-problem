b1daf1ef41a80793257159db5ccdb51fcce68331
carolineeey <esthermelia20@gmail.com>
2024-12-20 16:02:53 +0700

    solve two sum problem

---
+++ main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Two Sum
+	nums := []int{3, 2, 4}
+	target := 6
+	result := twoSumV2(nums, target)
+	fmt.Println(result)
+
+	//
+}
---
+++ main/two_sum.go
@@ -0,0 +1,29 @@
+package main
+
+func twoSumV1(nums []int, target int) (result []int) {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				result = []int{i, j}
+				return result
+			}
+		}
+	}
+	return result
+}
+
+func twoSumV2(nums []int, target int) (result []int) {
+	numIndices := make(map[int]int)
+	for i, num := range nums {
+		complement := target - num
+
+		if index, found := numIndices[complement]; found {
+			result = []int{index, i}
+			return
+		}
+
+		numIndices[num] = i
+	}
+
+	return result
+}
