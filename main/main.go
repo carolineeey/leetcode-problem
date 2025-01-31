a9705ed1e26e206ff51396c9ff790b02630741a7
carolineeey <esthermelia20@gmail.com>
2025-01-31 10:03:51 +0700

    Uncomment codes

--- main/main.go
+++ main/main.go
@@ -6,18 +6,18 @@ import (
 )
 
 func main() {
-	// 1. Two Sum
-	//nums := []int{3, 2, 4}
-	//target := 6
-	//result := solution.TwoSumV2(nums, target)
-	//fmt.Println(result)
-	//
-	//// 9. Palindrome Number
-	//isp := solution.IsPalindrome(123211)
-	//fmt.Println(isp)
+	//1. Two Sum
+	nums := []int{3, 2, 4}
+	target := 6
+	result := solution.TwoSumV2(nums, target)
+	fmt.Println(result)
+
+	// 9. Palindrome Number
+	isp := solution.IsPalindrome(123211)
+	fmt.Println(isp)
 
 	// 26. Remove duplicates from Sorted Array
-	nums := []int{5, 2, 3, 2, 4, 4}
+	nums = []int{5, 2, 3, 2, 4, 4}
 	length := solution.RemoveDuplicates(nums)
 	fmt.Println(length)
 }
