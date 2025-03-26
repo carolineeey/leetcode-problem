2ab4d29cab9c09a46fc8b1a6af07a11e22b86f30
carolineeey <esthermelia20@gmail.com>
2025-03-26 18:10:55 +0700

    Solve best time to buy and sell stock problem

--- solution/121.max_profit.go
+++ solution/121.max_profit.go
@@ -3,7 +3,7 @@ package solution
 func MaxProfit(prices []int) int {
 	minPrice := prices[0]
 	maxProfit := 0
-	//7, 6, 4, 3, 1
+
 	for i := 0; i < len(prices); i++ {
 		if prices[i] < minPrice {
 			minPrice = prices[i] //update min price if current value is less than minPrice as it will make minus profit
