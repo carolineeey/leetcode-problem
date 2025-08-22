3b993cfc56467884d2e175584cdd58f72111538c
carolineeey <esthermelia20@gmail.com>
2025-08-22 12:10:36 +0700

    Add documentation

--- solution/414.third_maximum_number.go
+++ solution/414.third_maximum_number.go
@@ -3,11 +3,13 @@ package solution
 import "sort"
 
 func ThirdMax(nums []int) int {
+	// use map to remove duplicate
 	unique := make(map[int]bool)
 	for _, num := range nums {
 		unique[num] = true
 	}
 
+	// append unique value to arr slice
 	arr := make([]int, 0, len(nums))
 	for num := range unique {
 		arr = append(arr, num)
@@ -15,8 +17,8 @@ func ThirdMax(nums []int) int {
 
 	sort.Sort(sort.Reverse(sort.IntSlice(arr)))
 	if len(arr) >= 3 {
-		return arr[2]
+		return arr[2] // third distinct max
 	}
 
-	return arr[0]
+	return arr[0] // max if less than 3
 }
