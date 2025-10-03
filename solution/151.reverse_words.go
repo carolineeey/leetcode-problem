6302b4378041ca69f9ba825c7aa9eb3c27ac8d25
carolineeey <esthermelia20@gmail.com>
2025-10-03 16:13:35 +0700

    Add solution for 151 problem

--- solution/151.reverse_words.go
+++ solution/151.reverse_words.go
@@ -8,11 +8,16 @@ func ReverseWords(s string) string {
 	words := strings.Fields(s)
 
 	// construct reverse words
-	reversed := make([]string, len(words))
-	for i, word := range words {
-		word = strings.TrimSpace(word)
-		reversed[len(words)-i-1] = word
+	//reversed := make([]string, len(words))
+	//for i, word := range words {
+	//	word = strings.TrimSpace(word)
+	//	reversed[len(words)-i-1] = word
+	//}
+
+	// reverse words using two-pointer for space efficient
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
 	}
 
-	return strings.Join(reversed, " ")
+	return strings.Join(words, " ")
 }
